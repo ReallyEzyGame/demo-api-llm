c5f316d43b33463843cfcbd6f631ff271377a6c0
ReallyEzyGame <lethanhphong0101016@gmail.com>
2026-04-10 21:46:50 +0700

    [UPDATE] including: adding comments to main.ipynb, clearer print output in localTest.py

--- source/localTest.py
+++ source/localTest.py
@@ -12,12 +12,11 @@ API_URL = "http://127.0.0.1:8000/"
 #
 parameters = {"prompt": "Thầy Lê Đức K ở trường Đại học Khoa học Tự nhiên có đẹp trai không?", "max_length": 512}
 # Kiểm tra 'post' method
-response = requests.post(API_URL + "generate", json=parameters)
-print(response.json())
+response1 = requests.post(API_URL + "generate", json=parameters)
+print(f'Kết quả của "get" post: {response1.json()}')
 # Kiểm tra 'get' method
 response2 = requests.get(API_URL)
-print(response2.json())
+print(f'Kết quả của "get": {response2.json()}')
 # kiểm tra 'get' health method
 response3 = requests.get(API_URL + "health")
-print(response3.json())
-
+print(f'Kết quả của "get" health: {response3.json()}')
